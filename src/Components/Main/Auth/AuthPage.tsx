52a88333b8621e6aad653c58ab17cf1acc23a655
marat25 <MaratTazhiev@yandex.ru>
2023-03-18 11:47:05 +0300

    4.0
    Исправил ошибку с фоном

--- src/Components/Main/Auth/AuthPage.tsx
+++ src/Components/Main/Auth/AuthPage.tsx
@@ -21,15 +21,20 @@ const AuthPage = () => {
     }
     // Проверка авторизации
     useEffect(() => {
-        let a = async () => {
-            let response = await AuthAPI.Logged()
-            if (response.status === 200) {
-                LocalStorage.setUserData(response.data.name, response.data.lastName, response.data.email)
-                LocalStorage.setToken(response.data.AccessToken)
-                LocalStorage.setIsAuthorized(true)
+        try {
+            let a = async () => {
+                let response = await AuthAPI.Logged()
+                if (response.status === 200) {
+                    LocalStorage.setUserData(response.data.name, response.data.lastName, response.data.email)
+                    LocalStorage.setToken(response.data.AccessToken)
+                    LocalStorage.setIsAuthorized(true)
+                }
             }
+            a()
+        } catch(e :any) {
+            SetMessageError(e.message)
         }
-        a()
+        
     }, [])
     // Форма логина
     const LoginFormAnt = () => {
